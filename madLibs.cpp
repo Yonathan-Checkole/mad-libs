37f3c3f2bed111a9aa56d120b6d9a97f3d9a0946
YonathanC <yonathancheckole@gmail.com>
2021-12-13 13:51:39 -0800

    done

---
+++ madLibs.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "Give me a verb: ";
+    std::string verb1;
+    std::cin >> verb1;
+
+    std::cout << "Give me a noun: ";
+    std::string noun1;
+    std::cin >> noun1;
+
+    std::cout << "Give me an adverb: ";
+    std::string adverb1;
+    std::cin >> adverb1;
+
+    std::cout << "Give me a verb (past tense): ";
+    std::string pastTenseVerb1;
+    std::cin >> pastTenseVerb1;
+
+    std::cout << "Give me a plural noun: ";
+    std::string pluralNoun1;
+    std::cin >> pluralNoun1;
+
+    std::cout << "Give me a plural noun: ";
+    std::string pluralNoun2;
+    std::cin >> pluralNoun2;
+
+    std::cout << "Give me an adjective: ";
+    std::string adjective1;
+    std::cin >> adjective1;
+
+    std::cout << "Give me a plural noun: ";
+    std::string pluralNoun3;
+    std::cin >> pluralNoun3;
+
+    std::cout << "Give me a verb ending in -ing: ";
+    std::string verb2;
+    std::cin >> verb2;
+
+    std::cout << "Give me a color: ";
+    std::string color1;
+    std::cin >> color1;
+
+    std::cout << "Give me a plural noun: ";
+    std::string pluralNoun4;
+    std::cin >> pluralNoun4;
+
+    std::cout << "Give me a noun: ";
+    std::string noun2;
+    std::cin >> noun2;
+
+    std::cout << "Give me a noun: ";
+    std::string noun3;
+    std::cin >> noun3;
+
+    std::cout << "Give me an adjective: ";
+    std::string adjective2;
+    std::cin >> adjective2;
+
+    std::cout << "Give me a verb: ";
+    std::string verb3;
+    std::cin >> verb3;
+
+    std::cout << "Give me an adjective: ";
+    std::string adjective3;
+    std::cin >> adjective3;
+
+    std::cout << "Give me an adjective: ";
+    std::string adjective4;
+    std::cin >> adjective4;
+
+    std::cout << "\nO say can you " << verb1 << ", by the dawn's early " << noun1 << ",\nWhat so " << adverb1 << " we " << pastTenseVerb1 << " at the twilight's last gleaming,\nWhose broad " << pluralNoun1 << " and bright " << pluralNoun2 << " through the " << adjective1 << " fight\nO'er the " << pluralNoun3 << " we watch'd were so gallantly " << verb2 << "?\nAnd the rocket's " << color1 << " glare, the " << pluralNoun4 << " bursting in " << noun2 << ",\nGave proof through the night that our " << noun3 << " was still there,\nO say does that " << adjective2 << " banner yet " << verb3 << "\nO'er the land of the " << adjective3 << " and the home of the " << adjective4 << "?\n";
+}
